e96e24035a57cb4bf35101f88e97e507b40d3b79
Eirik Birkeland <EirikBirkeland@users.noreply.github.com>
2016-04-06 01:27:26 +0200

    Removed tmp debugging

--- gitify.pl
+++ gitify.pl
@@ -2,12 +2,10 @@
 use strict;
 use warnings;
 use 5.10.1;
-use Data::Printer;
 
 if (scalar @ARGV != 2) {
       say "Invalid number of arguments."; exit 0;
 }
-p @ARGV; 
 
 system("git add $ARGV[0]");
 system("git commit -m $ARGV[1]");
